d725f0cafeca87b00fe166d93e989489d77590dc
Sauron754 <advancedcode754@gmail.com>
2017-04-15 01:25:42 +0200

    created redefineQuickCollisionBundingBox method for rectangle class

--- MechSim/objects/solids.py
+++ MechSim/objects/solids.py
@@ -29,25 +29,38 @@ class rectangle():
 		self.quickCollisionYDim = [self.yPointArray[0], self.yPointArray[7]]
 		self.quickCollisionZDim = [self.zPointArray[0], self.zPointArray[7]]
 
-	def setPoint(self, pos, posR, dim):
-		self.A = [((pos[0] - dim[0]/2) * math.cos(posR[0])), ((pos[1] - dim[1]/2) * math.cos(posR[1])), ((pos[2] - dim[2]/2) * math.cos(posR[2]))]
-		self.B = [((pos[0] + dim[0]/2) * math.cos(posR[0])), ((pos[1] - dim[1]/2) * math.cos(posR[1])), ((pos[2] - dim[2]/2) * math.cos(posR[2]))]
-		self.C = [((pos[0] - dim[0]/2) * maht.cos(posR[0])), ((pos[1] + dim[1]/2) * math.cos(posR[1])), ((pos[2] - dim[2]/2) * math.cos(posR[2]))]
-		self.D = [((pos[0] + dim[0]/2) * maht.cos(posR[0])), ((pos[1] + dim[1]/2) * math.cos(posR[1])), ((pos[2] - dim[2]/2) * math.cos(posR[2]))]
-		self.E = [((pos[0] - dim[0]/2) * maht.cos(posR[0])), ((pos[1] - dim[1]/2) * math.cos(posR[1])), ((pos[2] + dim[2]/2) * math.cos(posR[2]))]
-		self.F = [((pos[0] + dim[0]/2) * maht.cos(posR[0])), ((pos[1] - dim[1]/2) * math.cos(posR[1])), ((pos[2] + dim[2]/2) * math.cos(posR[2]))]
-		self.G = [((pos[0] - dim[0]/2) * maht.cos(posR[0])), ((pos[1] + dim[1]/2) * math.cos(posR[1])), ((pos[2] + dim[2]/2) * math.cos(posR[2]))]
-		self.H = [((pos[0] + dim[0]/2) * maht.cos(posR[0])), ((pos[1] + dim[1]/2) * math.cos(posR[1])), ((pos[2] + dim[2]/2) * math.cos(posR[2]))]
+	def setPoint(self):
+		self.A = [((self.pos[0] - self.dim[0]/2) * math.cos(self.posR[0])), ((self.pos[1] - self.dim[1]/2) * math.cos(self.posR[1])), ((self.pos[2] - self.dim[2]/2) * math.cos(self.posR[2]))]
+		self.B = [((self.pos[0] + self.dim[0]/2) * math.cos(self.posR[0])), ((self.pos[1] - self.dim[1]/2) * math.cos(self.posR[1])), ((self.pos[2] - self.dim[2]/2) * math.cos(self.posR[2]))]
+		self.C = [((self.pos[0] - self.dim[0]/2) * math.cos(self.posR[0])), ((self.pos[1] + self.dim[1]/2) * math.cos(self.posR[1])), ((self.pos[2] - self.dim[2]/2) * math.cos(self.posR[2]))]
+		self.D = [((self.pos[0] + self.dim[0]/2) * math.cos(self.posR[0])), ((self.pos[1] + self.dim[1]/2) * math.cos(self.posR[1])), ((self.pos[2] - self.dim[2]/2) * math.cos(self.posR[2]))]
+		self.E = [((self.pos[0] - self.dim[0]/2) * math.cos(self.posR[0])), ((self.pos[1] - self.dim[1]/2) * math.cos(self.posR[1])), ((self.pos[2] + self.dim[2]/2) * math.cos(self.posR[2]))]
+		self.F = [((self.pos[0] + self.dim[0]/2) * math.cos(self.posR[0])), ((self.pos[1] - self.dim[1]/2) * math.cos(self.posR[1])), ((self.pos[2] + self.dim[2]/2) * math.cos(self.posR[2]))]
+		self.G = [((self.pos[0] - self.dim[0]/2) * math.cos(self.posR[0])), ((self.pos[1] + self.dim[1]/2) * math.cos(self.posR[1])), ((self.pos[2] + self.dim[2]/2) * math.cos(self.posR[2]))]
+		self.H = [((self.pos[0] + self.dim[0]/2) * math.cos(self.posR[0])), ((self.pos[1] + self.dim[1]/2) * math.cos(self.posR[1])), ((self.pos[2] + self.dim[2]/2) * math.cos(self.posR[2]))]
 
-	def quickCollision(self, point):
+	def quickCollision(self, quickCollisionXDimB, quickCollisionYDimB, quickCollisionZDimB):
 		collision = False
-		if point[0] > self.quickCollisionXDim[0] and point[0] < self.quickCollisionXDim[1]:
-			if point[1] > self.quickCollisionYDim[0] and point[1] < self.quickCollisionYDim[1]:
-				if point[2] > self.quickCollisionZDim[0] and point[2] < self.quickCollisionZDim[1]:
+		if self.quickCollisionXDim[0] < quickCollisionXDimB[1] and self.quickCollisionXDim[1] > quickCollisionXDimB[0]:
+			if self.quickCollisionYDim[0] < quickCollisionYDimB[1] and self.quickCollisionYDim[1] > quickCollisionYDimB[0]:
+				if self.quickCollisionZDim[0] < quickCollisionZDim[1] and self.quickCollisionZDim[1] > quickCollisionZDimB[0]:
 					collision = True
 				else:
-					collision = False
+					pass
 			else:
 				pass
 		else:
 			pass
+		return collision
+
+	def redefineQuickCollisionBoundingBox(self):
+		setPoint()
+		self.xPointArray = [self.A[0], self.B[0], self.C[0], self.D[0], self.E[0], self.F[0], self.G[0], self.H[0]]
+		self.xPointArray.sort()
+		self.yPointArray = [self.A[1], self.B[1], self.C[1], self.D[1], self.E[1], self.F[1], self.G[1], self.H[1]]
+		self.yPointArray.sort()
+		self.zPointArray = [self.A[2], self.B[2], self.C[2], self.D[2], self.E[2], self.F[2], self.G[2], self.H[2]]
+		self.zPointArray.sort()
+		self.quickCollisionXDim = [self.xPointArray[0], self.xPointArray[7]]
+		self.quickCollisionYDim = [self.yPointArray[0], self.yPointArray[7]]
+		self.quickCollisionZDim = [self.zPointArray[0], self.zPointArray[7]]
